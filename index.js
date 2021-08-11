10ea69ec6953412a8f9c210f6a1deca82b3ee7eb
Hitesh Khode <khodehitesh@gmail.com>
2021-08-11 21:50:43 +0530

    initialstage

---
+++ index.js
@@ -0,0 +1,178 @@
+const express = require('express')
+const mysql = require('mysql');
+const crypto = require('crypto')
+const path = require('path');
+const bodyParser = require('body-parser');
+var userid;
+
+var userid
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+app.use(express.urlencoded())
+app.use(express.json())
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hitesh@2001',
+    database: 'signup'
+});
+db.connect((err) => {
+    if (err) throw err;
+    else console.log('connected to database')
+})
+app.get('/', (req, res) => {
+    console.log('opened signup page')
+    res.sendFile(__dirname + '//public/successfullogin.html')
+});
+function createsignuptable(req, res) {
+    const querytocreatesignuptable = 'CREATE TABLE signupmap (emailid NVARCHAR(320) UNIQUE NOT NULL, passwordofemailid VARCHAR(32) );'
+    db.query(querytocreatesignuptable, (errincreationsignupmap, result) => {
+        if (errincreationsignupmap) console.log(errincreationsignupmap.sqlMessage)
+    })
+}
+function emailappendingfunc(req, res) {
+    createsignuptable();
+    var emailidparsed = req.body.email;
+    var passwordparsed = crypto.createHash('md5').update(req.body.password).digest('hex');
+    var querytoaddemailandpass = "INSERT INTO signupmap (emailid, passwordofemailid) VALUES('" + emailidparsed + "', '" + passwordparsed + "');";
+    db.query(querytoaddemailandpass, (errinemailaddition, result) => {
+        if (errinemailaddition) res.send({ status: '!exist' })
+        res.send({ status: 'ok' })
+    })
+}
+function login(req, res) {
+    createsignuptable();
+    var passwordparsed = crypto.createHash('md5').update(req.body.password).digest('hex');
+    var querytologin = `SELECT passwordofemailid FROM signupmap WHERE emailid = '${req.body.email}';`
+    db.query(querytologin, (errinlogin, result) => {
+        // if(`${passwordparsed}` == result[0]['passwordofemailid']) res.sendFile(__dirname + '/public/successfullogin.html')
+        if (errinlogin) console.log(errinlogin.sqlMessage)
+        else if (result[0] == undefined) res.send({ status: '!exist' })
+        else if (`${passwordparsed}` == result[0]['passwordofemailid']) res.send({ userid, passwordparsed, status: 'ok' })
+        else res.send({ status: 'invalidcredentials' })
+    })
+}
+function teamcreation(params) {
+    querytocreateteam = 'CREATE TABLE teams();'
+}
+function createpolltable(question) {
+    questionhash = crypto.createHash('md5').update(question).digest('hex');
+    const querytocreatepolltable = 'CREATE TABLE `' + questionhash + '`(`' + question + '` VARCHAR(200), vote INT);'
+    db.query(querytocreatepolltable, (errorincreatepolltable, result) => {
+        if (errorincreatepolltable) console.log(errorincreatepolltable.sqlMessage)
+    })
+    const querytocreateallpollstable = `CREATE TABLE polls(pollid VARCHAR(32), admin NVARCHAR(230));`
+    db.query(querytocreateallpollstable, (errtocreateallpollstable, result) => {
+    })
+}
+function createpoll(req, res) {
+    userid = req.body.email
+    var questionhash = crypto.createHash('md5').update(req.body.question).digest('hex');
+    createpolltable(req.body.question)
+    db.query('INSERT INTO polls values("' + questionhash + '" , "' + userid + '")')
+    for (let i = 0; i < req.body.optioninput.length; i++) {
+        var querytoappendpolloptions = 'INSERT INTO `' + questionhash + '` VALUES("' + req.body.optioninput[i] + '", 0);'
+        db.query(querytoappendpolloptions, (errinappendingpolloptions, result) => {
+            if (errinappendingpolloptions) console.log(errinappendingpolloptions.sqlMessage)
+        })
+    }
+}
+function inviteuser(req, res) {
+    emailtoinvite = req.body.email
+    console.log('invite ran successfully ' + emailtoinvite + req.body.question)
+    questionhash = crypto.createHash('md5').update(req.body.question).digest('hex');
+    var querytocreateinvitetable = "CREATE TABLE `invites" + emailtoinvite + "`(invitedteam VARCHAR(32) UNIQUE);"
+    db.query(querytocreateinvitetable, (errortocreateinviteestable, result) => {
+        if (errortocreateinviteestable) console.log(errortocreateinviteestable.sqlMessage)
+    var querytoappendtoinviteestable = "INSERT INTO `invites" + emailtoinvite + "` VALUES('" + questionhash + "');"
+    db.query(querytoappendtoinviteestable, (errorinappendingtoinviteestable) => {
+        if(errorinappendingtoinviteestable) console.log("errorininvite" + errorinappendingtoinviteestable.sqlMessage)
+    })
+    })
+}
+
+function refreshinvitations(req, res) {
+    querytogetinvites = "SELECT * FROM `invites" + req.body.email + "`;"
+    var arrayofinvites = []
+    let counterone
+    let countertwo = 0
+    db.query(querytogetinvites, (errtogetinvites, result) => {
+        if(result != undefined){    
+            for (let i = 0; i < result.length; i++) {
+                counterone = result.length
+                var querytogetquestionname = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + result[i]['invitedteam'] + "' ORDER BY ORDINAL_POSITION;"
+                db.query(querytogetquestionname, (errtogetquestionname, result) => {
+                    if(errtogetquestionname) console.log(errtogetquestionname.sqlMessage)
+                    arrayofinvites.push(result[0].COLUMN_NAME)
+                    countertwo += 1
+                    if(countertwo == counterone) res.send({arrayofinvites})
+                })
+            }
+        }
+    })
+}
+function refreshingcreatedteams(req, res) {
+    email = req.body.email
+    arrayofcreatedpolls = []
+    let counterone;
+    let countertwo = 0;
+    querytogetcreatedteams = "SELECT pollid FROM polls WHERE admin = '" + email + "'";
+    db.query(querytogetcreatedteams, (errtogetcreatedteams, result) => {
+        if(errtogetcreatedteams) console.log(errtogetcreatedteams.sqlMessage);
+        counterone = result.length
+        for (let i = 0; i < result.length; i++) {
+            arrayofcreatedpolls.push(result[i].pollid)
+            countertwo += 1
+        }
+        if(countertwo == counterone) {
+            // res.send({arrayofcreatedpolls})
+            var arraytosend = []
+            let firstcounter = 0;
+            for (let i = 0; i < arrayofcreatedpolls.length; i++) {
+                querytogetquestiontable = "SELECT * FROM `" + arrayofcreatedpolls[i] + "`;"
+                db.query(querytogetquestiontable, (erringettingquestiontables, result) => {
+                    if(erringettingquestiontables) console.log(erringettingquestiontables.sqlMessage)
+                    arraytosend.push(result)
+                    firstcounter += 1
+                    if(firstcounter === arrayofcreatedpolls.length) res.send(arraytosend)
+                })
+            }
+        }
+    })
+}
+
+app.get('/createsignuptable', createsignuptable);
+app.post('/emailappendingfunc', emailappendingfunc);
+app.post('/login', login);
+app.post('/teamcreation', teamcreation)
+app.post('/createpoll', createpoll);
+app.post('/inviteuser', inviteuser);
+app.post('/refreshinvitations', refreshinvitations);
+app.post('/refreshingcreatedteams', refreshingcreatedteams)
+
+app.listen('3001', (req, res) => {
+    console.log('server started at 3001')
+});
+
+
+// function refreshinvitationspromise(req, res){
+//     return new Promise( (success, failure) => {
+//         querytogetinvites = "SELECT * FROM `invites" + req.body.email + "`;"
+//         console.log(querytogetinvites)
+//         var arrayofinvites = []
+//         db.query(querytogetinvites, (errtogetinvites, result) => {
+//             for (let i = 0; i < result.length; i++) {
+//                 var querytogetquestionname = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + result[i]['invitedteam'] + "' ORDER BY ORDINAL_POSITION;"
+//                 db.query(querytogetquestionname, (errtogetquestionname, result) => {
+//                     if(errtogetquestionname) console.log(errtogetquestionname.sqlMessage)
+//                     console.log(result)
+//                     arrayofinvites.push(result[0].COLUMN_NAME)
+//                 })
+//             }
+//         })
+//         setTimeout(() => {
+//             success(arrayofinvites)
+//         }, 1000);
+//     });
+// }
---
+++ public/successfullogin.js
@@ -0,0 +1,195 @@
+const pollcreationform = document.getElementsByClassName('pollcreationform')
+var counterofoptions = 0;
+const pollform = '<form action="/createpoll" method="post" id="pollcreationform">Question:<input type="text" name="question" id="pollcreationquestion">Options:<div id="options"><input type="text" name="optioninput" class="optioninput"></div><span id="optionaddition" onclick="addoption()">Add option</span><button type="submit">SUBMIT</button></form>'
+var email, password
+
+function navbarhideshower(){
+    if (document.getElementsByClassName('navbar')[0].getAttribute('id') == 'none') document.getElementsByClassName('navbar')[0].setAttribute('id', 'navbar');
+    else document.getElementsByClassName('navbar')[0].setAttribute('id', 'none');
+}
+function pollcreationformlaunch(){
+    counterofoptions = 0;
+    if (pollcreationform[0].getAttribute('id') == 'none'){
+        // document.querySelector('body').style.opacity = 0.3
+        pollcreationform[0].setAttribute('id', 'pollcreation');
+    }
+    else{
+        pollcreationform[0].setAttribute('id', 'none');
+        pollcreationform[0].removeChild(document.getElementById('pollcreationform'))
+        pollcreationform[0].innerHTML += pollform
+    }
+}
+function addoption() {
+    var input = document.createElement("input");
+    input.setAttribute("type", "option");
+    input.setAttribute("name", 'optioninput');
+    input.setAttribute("class", 'optioninput');
+    input.setAttribute("value", "");
+    document.getElementById('options').appendChild(input)
+    counterofoptions += 1
+}
+function refreshinvitations() {
+    document.getElementById('invitedteams').innerHTML = ''
+    fetch('/refreshinvitations', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email})}).then((response) => {
+            response.json().then((data) => {
+                appendquestiontoinvitearea(data.arrayofinvites, 'invitedteams')
+                console.log(data)
+            })
+        })
+    addbuttonstoinvite();
+}
+function refreshingcreatedteams() {
+    document.getElementById('createdteamtiles').innerHTML = ''
+    fetch('/refreshingcreatedteams', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            email
+        })
+    }).then((response) => {
+        response.json().then((data) => {
+            console.log(data)
+            appendquestiontocreatearea(data)
+        })
+    })
+}
+function appendquestiontocreatearea(data) {
+    arrayofkeys = []
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+            for(let k in data[i][j]){
+                arrayofkeys.push(k)
+                break
+            }
+        break
+        }
+    }
+    console.log(arrayofkeys.reverse())
+    appendquestiontoinvitearea(arrayofkeys, 'createdteamtiles')
+}
+function appendquestiontoinvitearea(arrayofinvites, divid) {
+    for (let i = 0; i < arrayofinvites.length; i++) {
+        var div = document.createElement('div');
+        div.className = divid;
+        div.innerText = arrayofinvites[i];
+        // div.oncl
+        document.getElementById(divid).appendChild(div);
+    }
+}
+function addbuttonstoinvite() {
+    arrayofinvitetiles = document.getElementsByClassName('invitedteams')
+    for (let i = 0; i < arrayofinvitetiles.length; i++) {
+        var buttonone = document.createElement('button');
+        var buttontwo = document.createElement('button');
+        buttonone.innerText = 'Accept';
+        buttontwo.innerText = 'Decline';
+        arrayofinvitetiles[i].appendChild(buttonone);
+        arrayofinvitetiles[i].appendChild(buttontwo);
+        console.log('addbutoon is running')
+    }
+}
+function teamcreationformlaunch() {
+    var statusofteamcreationform = document.getElementsByClassName('teamcreationform')[0].getAttribute('id')
+    if (statusofteamcreationform == 'none') document.getElementsByClassName('teamcreationform')[0].setAttribute('id', '')
+    else document.getElementsByClassName('teamcreationform')[0].setAttribute('id', 'none')
+}
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  THIS IF FOR TEAMCREATIONFORM
+function teamcreationform() {
+    fetch('/teamcreation')
+}
+
+
+
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  THIS IS CODE FOR LOGIN FORM
+
+var statuschecker = 'Login';
+function inchoser(status) {
+    statuschecker = status.innerText;
+    if (status.innerText === 'Sign up') {
+        document.getElementsByClassName('username')[0].setAttribute('id', '')
+    }
+    else {
+        document.getElementsByClassName('username')[0].setAttribute('id', 'displaynone')
+    }
+}
+document.getElementById('loginform').addEventListener('submit', (event) => {
+    event.preventDefault();
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+    if (statuschecker === 'Login') {
+        fetching('/login', email, password);
+    }
+    else fetching('/emailappendingfunc', email, password)
+})
+document.getElementById('pollcreationform').addEventListener('submit', (event) => {
+    event.preventDefault();
+    jsonforpollcreation = {}
+    jsonforpollcreation.question = document.getElementById('pollcreationquestion').value
+    jsonforpollcreation.email = email
+    jsonforpollcreation.optioninput = [];
+    for (let i = 0; i < document.getElementsByClassName('optioninput').length; i++) {
+        jsonforpollcreation.optioninput.push(document.getElementsByClassName('optioninput')[i].value)
+    }
+    jsonstring =  JSON.stringify(jsonforpollcreation)
+    fetch('/createpoll', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: jsonstring,});
+});
+
+document.getElementById('inviteuser').addEventListener('submit', (event) => {
+    event.preventDefault();
+    fetch('/inviteuser', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({question: document.getElementById('pollcreationquestion').value,
+            email: document.getElementById('guest').value
+        })
+    })
+})
+
+function fetching(endpoint, email, password) {
+    fetch(endpoint,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email, password}),
+        })
+        .then(response => response.json().then((data) => {
+                                            if (data.status == 'invalidcredentials') document.getElementById('msg').innerText = 'Invalid credentials'
+                                            else if (data.status == '!exist') {
+                                                if (endpoint === '/login') document.getElementById('msg').innerHTML = "Email doesn't exist"
+                                                else document.getElementById('msg').innerHTML = "Email exist"
+                                            }
+                                            else{
+                                                if (endpoint === '/login'){
+                                                    document.getElementById('msg').innerText = 'loggedin successfully'
+                                                    document.getElementsByClassName('signinbox')[0].setAttribute('id', 'none')
+                                                    document.getElementById('heading').innerText = 'YOUR LOGIN HAS BEEN SUCCESSFUL .....!'
+                                                    refreshinvitations();
+                                                    // refreshingcreatedteams();
+                                                }
+
+                                                else{
+                                                    inchoser(document.getElementById('login'))
+                                                    document.getElementById('msg').innerText = 'regestered successfully, now you can log in'
+                                                }
+                                            }
+        }));
+    
+}
